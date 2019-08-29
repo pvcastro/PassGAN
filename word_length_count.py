00cc25a8df0267eed41eb5ff553c5badaaef13ba
Pedro Vitor <pvcastro@gmail.com>
2019-08-29 19:07:22 -0300

    Adicionando escrita de senhas grandes para análise das senhas

--- word_length_count.py
+++ word_length_count.py
@@ -4,18 +4,35 @@ import fire
 from pprint import pprint
 
 
-def get_word_counts(file_path):
+def get_word_counts(file_path, big_path):
     d = defaultdict(int)
     file_path = Path(file_path)
+    big_path = Path(big_path)
 
-    with file_path.open(mode='r', encoding='utf8', errors='ignore') as file:
-        count_total = 0
-        count = 0
-        for idx, line in enumerate(file):
-            d[len(line)] += 1  # observe this bit carefully
-            count += 1
-            if count % 100000 == 0:
-                print('Counted %d lines in %s file' % (count, file_path.name))
+    with big_path.open(mode='w', encoding='utf8', errors='ignore') as out:
+
+        with file_path.open(mode='r', encoding='utf8', errors='ignore') as file:
+            count_written = 0
+            count = 0
+            for idx, line in enumerate(file):
+                size = len(line)
+                d[size] += 1  # observe this bit carefully
+
+                if size > 110:
+                    out.write(line + '\n\n')
+                    count_written += 1
+                    if count % 1000 == 0:
+                        print('Wrote %s' % line)
+
+                count += 1
+                if count % 100000 == 0:
+                    print('Counted %d lines in %s file' % (count, file_path.name))
+
+        print('Counted %d lines in %s file' % (count, file_path.name))
+    print('Wrote %d big passwords to %s file' % (count_written, big_path.name))
+
+    out.close()
+    file.close()
 
     pprint(d)
 
