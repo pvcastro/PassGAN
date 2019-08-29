a1d947e9e614be69e7528a3ed6c6db3127084974
Pedro Vitor <pvcastro@gmail.com>
2019-08-29 20:22:44 -0300

    Adicionando escrita de senhas normais

--- word_length_count.py
+++ word_length_count.py
@@ -4,18 +4,21 @@ import fire
 from pprint import pprint
 
 
-def get_word_counts(file_path, big_path, colon_path, lower_limit=100):
+def get_word_counts(file_path, big_path, colon_path, pwd_path, lower_limit=100):
     d = defaultdict(int)
     file_path = Path(file_path)
     big_path = Path(big_path)
     colon_path = Path(colon_path)
+    pwd_path = Path(pwd_path)
 
     colon_out = colon_path.open(mode='w', encoding='utf8', errors='ignore')
     big_out = big_path.open(mode='w', encoding='utf8', errors='ignore')
+    pwd_out = pwd_path.open(mode='w', encoding='utf8', errors='ignore')
 
     with file_path.open(mode='r', encoding='utf8', errors='ignore') as file:
         count_written = 0
         count_colon = 0
+        count_pwd = 0
         count = 0
         for idx, line in enumerate(file):
             size = len(line)
@@ -31,6 +34,11 @@ def get_word_counts(file_path, big_path, colon_path, lower_limit=100):
                 count_colon += 1
                 if count_colon % 1000 == 0:
                     print('Wrote with colon %s' % line)
+            else:
+                pwd_out.write(line)
+                count_pwd += 1
+                if count_pwd % 500000 == 0:
+                    print('Wrote normal password %s' % line)
 
             count += 1
             if count % 500000 == 0:
@@ -39,7 +47,9 @@ def get_word_counts(file_path, big_path, colon_path, lower_limit=100):
         print('Counted %d lines in %s file' % (count, file_path.name))
         print('Wrote %d big passwords to %s file' % (count_written, big_path.name))
         print('Wrote %d passwords with colon to %s file' % (count_colon, colon_path.name))
+        print('Wrote %d normal passwords to %s file' % (count_pwd, pwd_path.name))
 
+    pwd_out.close()
     colon_out.close()
     big_out.close()
     file.close()
