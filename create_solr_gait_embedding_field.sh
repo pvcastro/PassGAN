e158cc70427913ceac1b6caededb527c94d89bc8
Pedro Vitor <pvcastro@gmail.com>
2019-10-05 09:17:56 -0300

    Adicionando notebook com comparações entre embeddings

--- create_solr_gait_embedding_field.sh
+++ create_solr_gait_embedding_field.sh
@@ -4,11 +4,11 @@ export SOLR_HOME=/media/discoD/solr-8.2.0
 
 ./${SOLR_HOME}/bin/solr create -c gait
 
-curl -X POST -H 'Content-type:application/json' --data-binary '{"add-field":{"name":"gait_embedding_value","type":"gait_embedding_1024","stored":true }}' http://localhost:8983/solr/gait/schema
 curl -X POST -H 'Content-type:application/json' --data-binary '{"add-field-type":{"name":"embedding_1024","class":"solr.PointType","dimension":"1024","subFieldSuffix":"_d"}}' http://localhost:8983/solr/gait/schema
+curl -X POST -H 'Content-type:application/json' --data-binary '{"add-field":{"name":"gait_embedding_1024","type":"embedding_1024","stored":true }}' http://localhost:8983/solr/gait/schema
 
-curl -X POST -H 'Content-type:application/json' --data-binary '{"add-field":{"name":"gait_embedding_value","type":"gait_embedding_512","stored":true }}' http://localhost:8983/solr/gait/schema
 curl -X POST -H 'Content-type:application/json' --data-binary '{"add-field-type":{"name":"embedding_512","class":"solr.PointType","dimension":"512","subFieldSuffix":"_d"}}' http://localhost:8983/solr/gait/schema
+curl -X POST -H 'Content-type:application/json' --data-binary '{"add-field":{"name":"gait_embedding_512","type":"embedding_512","stored":true }}' http://localhost:8983/solr/gait/schema
 
-curl -X POST -H 'Content-type:application/json' --data-binary '{"add-field":{"name":"gait_embedding_value","type":"gait_embedding_128","stored":true }}' http://localhost:8983/solr/gait/schema
 curl -X POST -H 'Content-type:application/json' --data-binary '{"add-field-type":{"name":"embedding_128","class":"solr.PointType","dimension":"128","subFieldSuffix":"_d"}}' http://localhost:8983/solr/gait/schema
+curl -X POST -H 'Content-type:application/json' --data-binary '{"add-field":{"name":"gait_embedding_128","type":"embedding_128","stored":true }}' http://localhost:8983/solr/gait/schema
