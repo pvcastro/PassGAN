26fc144083fc0ec6e2363613d3507dbd361124d5
Pedro Vitor <pvcastro@gmail.com>
2019-11-27 14:53:35 -0300

    Adicionando método para avaliação a partir de um client do flask

--- password_evaluation.py
+++ password_evaluation.py
@@ -135,7 +135,7 @@ def evaluate_all_passwords(source_path, out_path, lms_path, max_length=20, batch
 def flask_evaluation(password: str, ngrams: int):
     params = {'password': password, 'ngrams': ngrams}
     response = requests.get('http://0.0.0.0:5002/evaluate', params=params)
-    print(smart_str(json.loads(response.text, encoding='utf8')))
+    print(json.loads(response.text, encoding='utf8').encode('utf8', errors='ignore'))
 
 
 if __name__ == '__main__': fire.Fire()
